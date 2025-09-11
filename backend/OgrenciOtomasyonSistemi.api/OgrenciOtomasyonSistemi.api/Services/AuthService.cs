53101af05c8c523e526fa6e0b0c02ab5ba98b727
Mustafa Talha Karagöz <mutakaragoz@gmail.com>
2025-09-11 15:46:39 +0300

    v1.3 Service klasörüyle kimlik doğrulama servisi (AuthService) eklendi. Kullanıcı kaydı (Register) ve girişi (Login) için temel iş olayı oluşturuldu. Bu servis, UserManager ile otomatik şekilde parolaları hashler. Başarılı girişte, kullanıcı kimlik ve rollerini içeren (Json Web Token) üretir ve return eder.

---
+++ backend/OgrenciOtomasyonSistemi.api/OgrenciOtomasyonSistemi.api/Services/AuthService.cs
@@ -0,0 +1,65 @@
+﻿namespace OgrenciOtomasyonSistemi.api.Services
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.IdentityModel.Tokens;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Security.Claims;
+    using System.Text;
+    using OgrenciOtomasyonSistemi.api.Entities;
+    public class AuthService
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthService(UserManager<AppUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        public async Task<IdentityResult> RegisterUserAsync(string email, string password, string role)
+        {
+            var user = new AppUser { UserName = email, Email = email };
+
+            var result = await _userManager.CreateAsync(user, password);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, role);
+            }
+            return result;
+        }
+        public async Task<string> LoginUserAsync(string email, string password)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user != null && await _userManager.CheckPasswordAsync(user, password))
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+                foreach (var userRole in userRoles)
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, userRole));
+                }
+
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+                var token = new JwtSecurityToken(
+                    issuer: _configuration["Jwt:Issuer"],
+                    audience: _configuration["Jwt:Audience"],
+                    expires: DateTime.Now.AddHours(3),
+                    claims: claims,
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                    );
+
+                return new JwtSecurityTokenHandler().WriteToken(token);
+            }
+            return null;
+        }
+    }
+}
+}
