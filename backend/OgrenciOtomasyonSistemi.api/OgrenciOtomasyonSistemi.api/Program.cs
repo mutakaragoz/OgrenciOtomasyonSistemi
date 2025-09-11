8595b42b09c153a7cd956d9374be24467d4ad9f7
Mustafa Talha Karagöz <mutakaragoz@gmail.com>
2025-09-11 21:08:55 +0300

    v1.5 Temel servisler ve altyapı yapılandırıldı Kullanıcı yönetimi için ASP.NET Core Identity servisleri kuruldu Güvenlik için JWT Bearer Authentication yapılandırıldı ve appsettings.json'a eklendi. Gerekli NuGet paketleri yüklendi.

--- backend/OgrenciOtomasyonSistemi.api/OgrenciOtomasyonSistemi.api/Program.cs
+++ backend/OgrenciOtomasyonSistemi.api/OgrenciOtomasyonSistemi.api/Program.cs
@@ -1,23 +1,57 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using OgrenciOtomasyonSistemi.api.Data;
+using OgrenciOtomasyonSistemi.api.Services;
+using System.Text;
+
+using static OgrenciOtomasyonSistemi.api.Data.AppDbContext;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
-// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = configuration["Jwt:Issuer"],
+        ValidAudience = configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
+    };
+});
+
+builder.Services.AddScoped<AuthService>();
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
-
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    await DataSeeder.SeedRolesAndAdminAsync(services);
+}
 
 app.Run();
